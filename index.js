40d464c8dcb684b43b9a6b1452063cd76005566b
Rohitilak <rohittilak086@gmail.com>
2024-05-20 12:23:30 +0530

    add

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  .connect("mongodb+srv://divyansh1:AyXWypDYdCjXpQSC@jobapp.ahokicb.mongodb.net/") // Connection String
+  .connect("mongodb+srv://rohittilak086:e9z4A2GX79VaoTcU@cluster0.c8bynpl.mongodb.net/") // Connection String
   .then(() => console.log("Connection with Database established successfully"))
   .catch((err) => console.log("ERROR CONNECTING WITH DATABASE", err));
 
 // API Routes
 app.use(jobRoutes);
 
-app.listen(100, () => console.log("Server is up and running at port 8080"));
+app.listen(1003, () => console.log("Server is up and running at port 8080"));
